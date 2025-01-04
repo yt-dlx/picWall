3aea8965d97e9965297e71f83f37724767ac0138
magneum-II <yt-dlx@proton.me>
2025-01-04 16:54:16 +0530

    picWall AI

--- backend/proc_7.js
+++ backend/proc_7.js
@@ -134,7 +134,7 @@ async function processDirectory(directory) {
       console.log(`Branch ${branchName} does not exist. Creating it now.`);
       await createBranch(branchName);
     }
-    const subdirectories = ["min"];
+    const subdirectories = ["max"];
     for (const subdir of subdirectories) {
       const subdirPath = join(directory, subdir);
       if (statSync(subdirPath).isDirectory()) {
