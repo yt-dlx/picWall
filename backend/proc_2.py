df47963adb4ec08022611d078392318827dfd69f
magneum-II <yt-dlx@proton.me>
2025-01-09 23:30:50 +0530

    com.shovit.picWall

--- backend/proc_2.py
+++ backend/proc_2.py
@@ -1,24 +1,15 @@
 import os
 from shutil import copy2
+from dotenv import load_dotenv
 from collections import defaultdict
+load_dotenv("../.env")
+prerem = os.getenv("PREREM", "").split(",")
 output_directory = os.path.join("sources", "input")
 input_directory = os.path.join("scripts", "Generated", "Images")
-prefixes_to_remove = [
-    "Leonardo Lightning XL", 
-    "Leonardo Anime XL", 
-    "Leonardo Vision XL", 
-    "Leonardo Kino XL",
-    "AlbedoBase XL",
-    "AlbedoBase_XL_",
-    "Leonardo_Kino_XL_",
-    "Leonardo_Anime_XL_", 
-    "Leonardo_Vision_XL_", 
-    "Leonardo_Lightning_XL_", 
-]
 def clean_base_name(filename):
-    for prefix in prefixes_to_remove:
-        if filename.startswith(prefix):
-            filename = filename.replace(prefix, "").strip()
+    for prefix in prerem:
+        if filename.startswith(prefix.strip()):
+            filename = filename.replace(prefix.strip(), "").strip()
             break
     if "(" in filename and ")" in filename:
         filename = filename.rsplit("(", 1)[0].strip()
@@ -43,4 +34,4 @@ for root, _, files in os.walk(input_directory):
             new_name = generate_new_name(group)
             new_file = os.path.join(target_folder, new_name)
             copy2(old_file, new_file)
-            print(f'Done: {new_name} (in) {target_folder}"')
+            print(f"Done: {new_name} (in) {target_folder}")
